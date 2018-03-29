cc80a0959087f57d2de3440f8f71f0d2511ca8d9
albertoacn <alberto@tokbox.com>
2018-03-29 17:50:09 +1100

    Add script to enable consent in MS Edge as non admin

---
+++ edge-setup/enableConsentEdge.bat
@@ -0,0 +1,20 @@
+@PowerShell.exe -ExecutionPolicy RemoteSigned -Command "Invoke-Expression -Command ((Get-Content -Path '%~f0' | Select-Object -Skip 2) -join [environment]::NewLine)"
+@exit /b %Errorlevel%
+#Install scoop
+set-executionpolicy unrestricted -s cu -f
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+scoop install sudo
+
+# Add Enable Consent to Windows Registry
+$registryPath = "HKLM:\Software\Microsoft\MicrosoftEdge"
+$registryPathItem = "$($registryPath)\MediaCapture"
+$name = "EnableConsentPrompt"
+$value = "0"  
+
+echo "Adding enable consent to Windows registry..."
+
+sudo New-Item -Path $registryPath -Force | Out-Null   
+sudo New-Item -Path $registryPathItem -Force | Out-Null    
+sudo New-ItemProperty -Path $registryPathItem -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+
+echo "Success!"
